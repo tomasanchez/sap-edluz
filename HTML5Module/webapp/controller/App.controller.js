53af961ec5811f88c68ba16bd38b78fd026400ff
Tomas <tosanchez@est.frba.utn.edu.ar>
2020-10-22 20:04:20 +0000

    Added home view

--- HTML5Module/webapp/controller/App.controller.js
+++ HTML5Module/webapp/controller/App.controller.js
@@ -1,15 +1,23 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
 	],
-	/**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-	function (Controller) {
+	function (Controller, History) {
 		"use strict";
 
 		return Controller.extend("ns.HTML5Module.controller.App", {
-			onInit: function () {
-
+            getRouter: function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		onNavBack: function (oEvent) {
+			var oHistory, sPreviousHash;
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("appHome", {}, true /*no history*/ );
 			}
+		}
 		});
 	});
